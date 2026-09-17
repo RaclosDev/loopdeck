888ad2627ab32b69333c65741e57a8fedcd51d7d
raclosdev <raclosdev@gmail.com>
2026-09-17 22:46:58 +0200

    fix(pwa): stop ignoring public/sw.js in git so CI can build injectManifest

---
+++ public/sw.js
@@ -0,0 +1,13 @@
+﻿import { precacheAndRoute } from 'workbox-precaching';
+
+// Precache resources injected by VitePWA
+precacheAndRoute(self.__WB_MANIFEST);
+
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
